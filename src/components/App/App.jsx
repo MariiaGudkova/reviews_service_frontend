6078e715b3e42121fd512a1745641adf40fb8e63
Gudkova Mariia <gudmary8@gmail.com>
2023-01-11 13:24:33 +0400

    add routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import { routes } from "../../utils/routes";
 import Header from "../Header/Header";
 
 function App() {
-  return <Header />;
+  const [isLogged, setIsLogged] = React.useState(false);
+  const [isAdmin, setIsAdmin] = React.useState(false);
+
+  return (
+    <Switch>
+      <Route exact path={routes.baseRoute}>
+        <Header isLogged={isLogged} isAdmin={isAdmin} />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/utils/routes.js
@@ -0,0 +1,13 @@
+export const routes = {
+  baseRoute: '/',
+  signIn: '/sign-in',
+  signUp: '/sign-up',
+  profile: '/profile',
+  games: '/games',
+  films: '/films',
+  serials:'/serials',
+  anime: '/anime',
+  books: '/books',
+  allUsers: '/allUsers',
+  notFound: 'notFound',
+}
