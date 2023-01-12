c3cb95be04f0921d09e7a6951688457893d4a758
Gudkova Mariia <gudmary8@gmail.com>
2023-01-13 00:39:02 +0400

    add test cards info

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+import chainsawmanImg from "../images/chainsawman.jpg";
+import cyberpunk2077Img from "../images/cyberpunk2077.jpg";
+import separationImg from "../images/separation.jpg";
+import dragonhouseImg from "../images/dragonhouse.jpg";
+import avatarImg from "../images/avatar2.jpg";
+import warGodsImg from "../images/god-of-war-ragnarok.jpg";
+import womansImg from '../images/womansbook.jpg';
+import murakamiImg from '../images/murakamibook.jpg';
+
+
+
+export const tags = ['#2023', '#2022', '#драма', '#супергерои', '#киберпанк', '#будущее', '#боевик', '#Netflix', '#приключения', '#фантастика', '#Marvel', '#манга', '#меха', '#сёнэн', '#мелодрама', '#русскиесериалы', '#криминал', '#комедиизарубежные', '#лучшиекниги', '#топлучшихкниг', '#психология', '#личностныйрост', '#мотивация', '#психологияотношений', '#топаниме', '#анимефэнтези', '#зарубежныебоевики', '#триллеры2022', '#любовь'];
+
+export const popularReviews = [{id: 1, title: 'Человек-бензопила', averageValue: 4.6, author: 'vasya edadil',  likes: 5, dislikes: 1, views: 24, cover: chainsawmanImg }, {id: 2, title: 'Cyberpunk 2077', averageValue: 4.5, author: 'Кот Матроскин', likes: 6, dislikes: 2, views: 15, cover: cyberpunk2077Img }, {id: 3, title: 'Разделение', averageValue: 4.8, author: 'проходимец', likes: 4, dislikes: 0, views: 30, cover: separationImg }, {id: 4, title: 'Дом Дракона', averageValue: 5, author: 'соленый огурец', likes: 10, dislikes: 1, views: 50, cover: dragonhouseImg }, {id: 6, title: 'Аватар 2', averageValue: 4.3, author: 'аватарец', likes: 2, dislikes: 0, views: 10, cover: avatarImg }, {id: 7, title: 'God of War: Ragnarök', averageValue: 4.5, author: 'пельмень', likes: 8, dislikes: 1, views: 30, cover: warGodsImg }, {id: 8, title: '52 упрямые женщины', averageValue: 4.1, author: 'женьщина', likes: 4, dislikes: 0, views: 8, cover: womansImg }, {id: 9, title: 'От первого лица', averageValue: 4.7, author: 'люська', likes: 10, dislikes: 1, views: 22, cover: murakamiImg } ];
